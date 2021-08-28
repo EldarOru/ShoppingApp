9d2e4baf6adb7c0655e4e7b18e982b0a35785435
EldarOru <macdog108@gmail.com>
2021-08-29 00:22:59 +0300

    finished

--- app/src/main/java/com/example/shoppingapp/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppingapp/data/ShopListRepositoryImpl.kt
@@ -5,8 +5,14 @@ import com.example.shoppingapp.domain.ShopItemRepository
 import java.util.*
 
 object ShopListRepositoryImpl: ShopItemRepository {
+
     private val shopList:MutableList<ShopItem> = ArrayList()
+
+    private var autoIncrementId = 0
     override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID){
+            shopItem.id = autoIncrementId++
+        }
         shopList.add(shopItem)
     }
 
@@ -15,17 +21,19 @@ object ShopListRepositoryImpl: ShopItemRepository {
     }
 
     override fun changeShopItem(shopItem: ShopItem) {
-
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
     }
 
-    TODO()
-    override fun getShopItem(shopItemID: Int): ShopItem {
-        for(shopItem in shopList){
-            if (shopItem.id == shopItemID){
-                return shopItem
-            }else throw RuntimeException ("")
+    override fun getShopItem(shopItemID: Int): ShopItem{
+        var shopItem:ShopItem? = null
+        for (ob in shopList){
+            if (ob.id == shopItemID){
+                shopItem == ob
+            }
         }
-        return null
+        return shopItem ?: throw Exception ("Element with id $shopItemID is not existed")
     }
 
     override fun getShopList(): List<ShopItem> {
